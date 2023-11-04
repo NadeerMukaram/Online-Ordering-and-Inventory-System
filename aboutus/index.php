858661b78528bfe80e857eae83072f0b8e7fc1f7
Nadeer Mukaram <72739154+NadeerMukaram@users.noreply.github.com>
2023-11-04 15:45:35 +0800

    Add files via upload

---
+++ aboutus/index.php
@@ -0,0 +1,331 @@
+<html>
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>About Us</title>
+<link rel="icon" href="../img/tabicon.png" type="image/gif" sizes="16x16">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">	
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">		
+<link href="http://fonts.cdnfonts.com/css/evogria" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@600&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+
+
+<style>
+
+@import url('http://fonts.cdnfonts.com/css/evogria');
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+	font-family: 'Evogria', sans-serif;
+}
+
+
+div,
+input,
+p,
+a {
+    font-family: 'Titillium Web', sans-serif;
+    margin: 0px;
+}
+
+a,
+a:hover,
+a:focus {
+    color: inherit;
+}
+
+img {
+    image-rendering: -webkit-optimize-contrast;
+}
+
+.container-fluid,
+.container {
+    max-width: 1200px;
+}
+
+.card-container {
+    padding: 100px 0px;
+    -webkit-perspective: 1000;
+    perspective: 1000;
+}
+
+.profile-card-2 {
+    max-width: 300px;
+    background-color: #FFF;
+    box-shadow: 0px 0px 25px rgba(0, 0, 0, 0.1);
+    background-position: center;
+    overflow: hidden;
+    position: relative;
+    margin: 10px auto;
+    cursor: pointer;
+    border-radius: 10px;
+}
+
+.profile-card-2 img {
+	height:300px;
+    transition: all linear 0.25s;
+	padding: 5px;
+	border-radius: 12px;
+}
+
+.profile-card-2 .profile-name {
+    position: absolute;
+    left: 30px;
+    bottom: 70px;
+    font-size: 20px;
+    color: #FFF;
+    text-shadow: 0px 0px 20px rgba(0, 0, 0, 0.5);
+    font-weight: bold;
+    transition: all linear 0.25s;
+}
+
+.profile-card-2 .profile-icons {
+    position: absolute;
+    bottom: 30px;
+    right: 30px;
+    color: #658AAF;
+    transition: all linear 0.25s;
+}
+
+.profile-card-2 .profile-username {
+    position: absolute;
+    bottom: 50px;
+    left: 30px;
+    color: #333333;
+    font-size: 15px;
+    transition: all linear 0.25s;
+    background: white;
+    padding: 1px 5px;
+    border-radius: 5px;
+}
+
+.profile-card-2 .profile-icons .fa {
+    margin: 5px;
+}
+
+.profile-card-2:hover img {
+    filter: grayscale(100%);
+}
+
+.profile-card-2:hover .profile-name {
+    bottom: 80px;
+}
+
+.profile-card-2:hover .profile-username {
+    bottom: 60px;
+}
+
+.profile-card-2:hover .profile-icons {
+    right: 40px;
+}
+
+.aboutsbody {
+	margin-top:1%;
+	margin-bottom: 5%;
+}
+
+body {
+	background-image: linear-gradient(to top, #deded6 , #c8c6a7, #c8c6a7);
+}
+
+
+
+
+.profiledes {
+    padding: 5px 10px;
+    width: 83%;
+    margin: auto;
+    border-radius: 10px;
+    font-size: 20px;
+	color:white;
+	background-image:url("https://img.freepik.com/free-photo/wooden-background_24972-623.jpg?size=626&ext=jpg");
+	background-size:100% 100%;
+}
+
+.profiledess {
+    padding: 5px 10px;
+    width: 83%;
+    margin: auto;
+    border-radius: 10px;
+    font-size: 20px;
+	color:white;
+	background-image:url("https://img.freepik.com/free-photo/wooden-background_24972-623.jpg?size=626&ext=jpg");
+	background-size:100% 100%;
+}
+
+.anothawan {
+    border-left: solid #333 5px;
+    padding: 8px;
+}
+
+
+
+.footerforlogin {
+    position: relative;
+    margin-top: 200px;
+    padding: 70px;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    color: #333333;
+    text-align: center;
+}
+
+@media  screen and (min-width:1024px) {
+    
+    .position {
+	position:absolute;
+	top:20%;
+	left:0;
+	font-size:150px;
+    }
+    
+    .imgposition {
+    margin: auto;
+    width: 55%;
+    display: block;
+    padding: 40px;
+    }
+
+    
+    .nonedisplay {
+    display:block;
+    }
+}
+
+@media  screen and (max-width:800px) {
+    
+    .position {
+	position:absolute;
+	top:10%;
+	left:0;
+	font-size:50px;
+    }
+    
+    .imgposition {
+    margin: auto;
+    width: 85%;
+    display: block;
+    padding: 40px;
+    }
+
+    
+    .nonedisplay {
+    display:none;
+    }
+}
+
+</style>
+
+</head>
+
+<body>
+
+<a href="https://hantechdesign.com/home/"><img class="imgposition" src="https://hantechdesign.com/img/hantech_official_logo_ok_alt.png"></a>
+
+
+<a href="https://hantechdesign.com/home/"><i class="fas fa-angle-double-left position"></i></a>
+
+<div class="container aboutsbody">
+
+	<div class="row">
+	
+	<div class="col-md-4">
+		<div class="profile-card-2"><img src="https://scontent.fmnl25-2.fna.fbcdn.net/v/t1.6435-9/103362777_3665253643490760_2296477436123806917_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeEltBLDWxe3D1Gcl0UqN_TRcUcw-SXpIElxRzD5JekgSUPUsXtLikmFRunYsFm-jCauPMqRws2ZubECBEsAmyz8&_nc_ohc=IRPbzTheIwIAX9js-5U&_nc_ht=scontent.fmnl25-2.fna&oh=1edc8935525fd2f75476d46a26672933&oe=60DFC327" class="img img-responsive">
+        <h1 class="profile-name">Nadeer Mukaram</h1>
+        <div class="profile-username">Designer</div>
+        <div class="profile-icons"><a href="https://www.facebook.com/nzro3"><i class="fab fa-facebook" style="font-size:30px;"></i></a></div>
+		</div>
+		<div class="profiledes">
+		<div class="anothawan">
+		Is the one responsible for the coding and creating the design and layout of the web system on the user side.
+		</div>
+		</div>
+	</div>
+	
+	<div class="col-md-4">
+	    <div class="profile-card-2"><img src="https://i.imgur.com/SwwiqhB.jpg" class="img img-responsive">
+		<h1 class="profile-name">Warren Josiah de Guzman</h1>
+        <div class="profile-username">Project Manager</div>
+        <div class="profile-icons"><a href="https://www.facebook.com/warrenjosiah.deguzman.7"><i class="fab fa-facebook" style="font-size:30px;"></i></a></div>
+		</div>
+		<div class="profiledes">
+		<div class="anothawan">
+		 Is the leader of the Han Tech Group and assigned to plan, build, develop, and manage the website. 
+		</div>
+		</div>
+	</div>
+	
+	<div class="col-md-4">
+	    <div class="profile-card-2"><img src="https://scontent.fmnl25-1.fna.fbcdn.net/v/t1.6435-1/p200x200/59894630_829271434074266_1187012180453621760_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=7206a8&_nc_eui2=AeEWR5iT8IHC27OiDD1v0sWiv-n0sxwYiK2_6fSzHBiIrSFKNlMbXCnyq9Pzr9_tgWgLFeMCmMLxo_S5yCKw8NTw&_nc_ohc=IJx3xldG7IEAX8WdtyN&_nc_ht=scontent.fmnl25-1.fna&tp=6&oh=302ee7ebd393dc23d04369ffcb8c0e8f&oe=60DEA197" class="img img-responsive">
+        <h1 class="profile-name">Reenjay Caimor</h1>
+        <div class="profile-username">Developer</div>
+        <div class="profile-icons"><a href="https://www.facebook.com/reenjie.caimor"><i class="fab fa-facebook" style="font-size:30px;"></i></a></div>
+		</div>
+		<div class="profiledes">
+		<div class="anothawan">
+		Is responsible for designing, coding and modifying the web system, from layout to function for both user and admin side and is the vital member of the group.
+		</div>
+		</div>
+	</div>
+	
+	</div>
+	
+	<div class="row" style="margin-top:5%;">
+	
+	<div class="col-md-4">
+		<div class="profile-card-2"><img src="https://scontent.fmnl25-2.fna.fbcdn.net/v/t1.6435-9/103339597_3215912611809268_1237536465372620151_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeFqG98vKe3kQFmnvUYFlxS3H65EMjXsxvUfrkQyNezG9TCcsqzr8QNvExFKlWkIhZn3kXD2qgYly9qrebbX5BRY&_nc_ohc=ie8KeqUfgz0AX92Kw4P&_nc_ht=scontent.fmnl25-2.fna&oh=b80a897316169ff6fc02c0abc927cad0&oe=60DD8774" class="img img-responsive">
+        <h1 class="profile-name">Ryan Peñosa Camonias</h1>
+        <div class="profile-username">Analysts/Tester</div>
+        <div class="profile-icons"><a href="https://www.facebook.com/ryan.camonias"><i class="fab fa-facebook" style="font-size:30px;"></i></a></div>
+		</div>
+		<div class="profiledess">
+		<div class="anothawan">
+		 Is responsible for analyzing the website's design and generating insightful analytics.
+		</div>
+		</div>
+	</div>
+	
+	<div class="col-md-4 nonedisplay">
+	   <img src="https://i.imgur.com/qEH8Bls.png" class="img img-responsive">
+	</div>
+	
+	<div class="col-md-4">
+	    <div class="profile-card-2"><img src="https://scontent.fmnl25-2.fna.fbcdn.net/v/t1.6435-9/181949612_1185464581890507_4100989031886941123_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeGFNN74L5v7Zv38z_k3HLz_8a72dgNCFxjxrvZ2A0IXGJJkqNhIUGe6lDi3xwG43g6DKlBVQbmJsY6F4_-ZR1K9&_nc_ohc=ImWwxWAVhB0AX_TaOwt&_nc_ht=scontent.fmnl25-2.fna&oh=bf26db205e50086ff3ca582801f5fdd9&oe=60DF7E07" class="img img-responsive">
+        <h1 class="profile-name">Kerzen Dalman Lañojan</h1>
+        <div class="profile-username">Analysts/Tester</div>
+        <div class="profile-icons"><a href="https://www.facebook.com/kerzen.lanojan.13"><i class="fab fa-facebook" style="font-size:30px;"></i></a></div>
+		</div>
+		<div class="profiledess">
+		<div class="anothawan">
+		 Is also responsible for analyzing the website's design and reporting back with easy-to-understand data visualizations.
+		</div>
+		</div>
+	</div>
+	
+	
+	</div>
+	
+</div>
+
+<div class="footerforlogin">
+	<div class="row">
+ 
+		<div class="col-md-12">
+			<hr>
+			<h4>Eldreanne Online Ordering and Inventory System © 2021</h4>
+			<h6>Warren Josiah de Guzman I Nadeer Mukaram I Ryan Peñosa Camonias I Reenjay Caimor I Kerzen Dalman Lañojan</h6>		
+		</div>
+	
+	</div>	
+</div>
+
+</body>
+
+</html>
